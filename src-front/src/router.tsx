655f095e8d020544b2b20bef7ff468bbf22c0493
Charly Schmidt alias Picorims <picorims.contact@gmail.com>
2024-11-18 18:39:02 +0100

    🐳 chore(front): add router

---
+++ src-front/src/router.tsx
@@ -0,0 +1,22 @@
+/*
+  Copyright (c) 2024 Charly Schmidt aka Picorims<picorims.contact@gmail.com>,
+  Alexis Hu, Olivier Proquez, Arnaud Bulteau, Maxime Le Bot
+
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+        path: "/about",
+        element: <h1>About</h1>,
+    }
+]);
