256ab79ee95f5dc01bbdc39c9348ca5b9ed997fc
nano1342 <arnaudbulteau@yahoo.fr>
2024-11-20 15:48:17 +0100

    fixed the size of the canvas

--- src-front/src/components/WordCloud/WordCloud.tsx
+++ src-front/src/components/WordCloud/WordCloud.tsx
@@ -1,13 +1,22 @@
-import { useCallback, useEffect, useRef } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { Application, Assets, Sprite } from 'pixi.js';
 
 export default function WordCloud() {
   const refCanvas = useRef<HTMLDivElement>(null);
+  const refContainer = useRef<HTMLDivElement>(null);
+  const [containerSize, setContainerSize] = useState({ width:0, height:0 })
   
   const init = useCallback(async () => {
     const app = new Application();
 
-    await app.init({ background: '#1099bb', resizeTo: window });
+    if (refContainer.current) {
+      await app.init({
+        background: '#1099bb',
+        resizeTo: refContainer.current,
+        width:containerSize.width,
+        height:containerSize.height
+      });
+    }
     app.canvas.id = "pixi-canvas";
     
     if (refCanvas.current) {
@@ -30,7 +39,7 @@ export default function WordCloud() {
     });
 
     return app;
-  }, [])
+  }, [containerSize])
 
   //cleanup function that erases the canvas when the page unmounts
   useEffect(() => {
@@ -48,8 +57,26 @@ export default function WordCloud() {
       })
     }
   }, [init]);
+  
+  //function that updates the size of the container
+  useEffect(() => {
+    const updateSize = () => {    //updates the size according to the window
+      if (refContainer.current) {
+        setContainerSize({
+          width:refContainer.current.clientWidth,
+          height:refContainer.current.clientHeight
+        });
+      }
+    };
+    window.addEventListener("resize", updateSize)   //the listener calls the previous function
+    updateSize();
+
+    return () => window.removeEventListener("resize", updateSize);
+  }, []);
 
   return <>
-  <div ref={refCanvas}></div>
+    <div ref={refContainer} style={{ width: "700px", height: "700px" }}>
+      <div ref={refCanvas}></div>
+    </div>
   </>;
 }
