eb93e8feab420be151800f5a636941d6096a3090
Charly Schmidt alias Picorims <picorims.contact@gmail.com>
2024-11-20 17:53:56 +0100

    ✨ feat(project): setup basic websocket example

---
+++ src-back/web_socket.py
@@ -0,0 +1,20 @@
+"""
+Copyright (c) 2024 Charly Schmidt aka Picorims<picorims.contact@gmail.com>,
+Alexis Hu, Olivier Proquez, Arnaud Bulteau, Maxime Le Bot
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+"""
+
+from fastapi import WebSocket
+from fastapi.responses import HTMLResponse
+from main import app
+
+
+@app.websocket("/api/v1/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
