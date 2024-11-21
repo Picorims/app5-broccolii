9b83a97335efcd087333f95fb6264996a282523f
KFCSama <hualexis951@gmail.com>
2024-11-21 11:48:03 +0100

    ✨ feat(clicker): display BoosterStats when BoosterCard onClick

--- src-front/src/components/BoosterCard/BoosterCard.tsx
+++ src-front/src/components/BoosterCard/BoosterCard.tsx
@@ -1,10 +1,34 @@
+import React, { useState } from "react";
 import styles from "./BoosterCard.module.css"; // Importation du fichier CSS pour la mise en page.
+
 import beetrootImage from "../../assets/boosterCard/beetrootImage.png";
+import beetrootStatsImage from "../../assets/boosterCard/beetrootStatsImage.png"; // Importation de l'image des statistiques
 
 const BoosterCard: React.FC<{ onClick: () => void }> = ({ onClick }) => {
+  const [isStatsVisible, setIsStatsVisible] = useState(false); // État pour afficher/cacher les statistiques
+
+  const handleImageClick = () => {
+    setIsStatsVisible((prev) => !prev); // Inverse la visibilité à chaque clic
+    onClick(); // Appelle la fonction onClick passée en props
+  };
+
   return (
-    <div className={styles.boostercard} onClick={onClick}>
-      <img src={beetrootImage} alt="Booster" className={styles.boosterImage} />
+    <div>
+      {/* BoosterCard */}
+      <div className={styles.boostercard} onClick={handleImageClick}>
+        <img src={beetrootImage} alt="Booster" className={styles.boosterImage} />
+      </div>
+
+      {/* Image des statistiques */}
+      {isStatsVisible && (
+        <div className={styles.statsContainer}>
+          <img
+            src={beetrootStatsImage}
+            alt="Beetroot Stats"
+            className={styles.statsImage}
+          />
+        </div>
+      )}
     </div>
   );
 };
