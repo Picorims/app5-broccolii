2af6804498751de6de3f4beda6f3a4d6e8bd3f1f
Charly Schmidt alias Picorims <picorims.contact@gmail.com>
2024-12-25 18:32:44 +0100

    🌈 style(front): format

--- src-front/src/lib/api.ts
+++ src-front/src/lib/api.ts
@@ -98,7 +98,6 @@ export class API {
     localStorage.removeItem("refresh_token");
   }
 
-
   public static getCurrentUserInfo() {
     const url = `${getEnv().backendUrl}/api/v1/user/me`;
     console.log("[API] [GET] " + url);
@@ -111,7 +110,7 @@ export class API {
       },
     });
   }
-  
+
   public static async createFight(players_list: string[]) {
     const url = `${getEnv().backendUrl}/api/v1/fight/create`;
 
