a592f8682d659db4fccab125af06f160999fba9a
Olivier Proquez <olivier.proquez@gmail.com>
2024-12-26 23:05:36 +0100

    🐞 fix(api.tsx): add methods from branch 103 as git is confused and creates conflicts
    the function unequipCard is merged by git with my function patchClick...

--- src-front/src/lib/api.ts
+++ src-front/src/lib/api.ts
@@ -124,39 +124,6 @@ export class API {
       }),
     });
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   
   public static patchClick() {
     const url = `${getEnv().backendUrl}/api/v1/click`;
@@ -170,5 +137,50 @@ export class API {
       },
     });
   }
-}
 
+  public static async addCard(username: string, cardId: string) {
+    const url = `${getEnv().backendUrl}/api/v1/card/add`;
+
+    return fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        cardId: cardId,
+      }),
+    });
+  }
+
+  public static async equipCard(username: string, cardId: string) {
+    const url = `${getEnv().backendUrl}/api/v1/card/equip`;
+
+    return fetch(url, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        cardId: cardId,
+      }),
+    });
+  }
+
+  public static async unequipCard(username: string, cardId: string) {
+    const url = `${getEnv().backendUrl}/api/v1/card/unequip`;
+
+    return fetch(url, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        cardId: cardId,
+      }),
+    });
+  }
+
+}
