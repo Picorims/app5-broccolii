4fd98cfb3ec80433eed3b3c5f7191e708a363929
Charly Schmidt alias Picorims <picorims.contact@gmail.com>
2024-12-30 22:42:38 +0100

    🐞 fix(word cloud): try to improve initialization

--- src-front/src/components/WordCloud/WordCloud.tsx
+++ src-front/src/components/WordCloud/WordCloud.tsx
@@ -182,9 +182,13 @@ export default function WordCloud({
   const refDisplayBestProgress =
     useRef<(wordsBestProgress: Record<string, number>) => void>();
   const refPreviousEntry = useRef<string>("");
+  const [pixiReady, setPixiReady] = useState(false);
+  const [webSocketReady, setWebSocketReady] = useState(false);
 
   //WordCloud initialization
   const init = useCallback(async () => {
+    console.log("Initializing Pixi.JS...");
+    
     const app = new Application();
 
     if (refContainer.current) {
@@ -196,13 +200,13 @@ export default function WordCloud({
       });
     }
     app.canvas.id = "pixi-canvas";
-
+    
     clearWords(); //deletes words from previous renders
-
+    
     if (refCanvas.current) {
       refCanvas.current.appendChild(app.canvas);
     }
-
+    
     //setApp(app);
     refApp.current = app;
 
@@ -249,6 +253,7 @@ export default function WordCloud({
     });
 
     updateSize();
+    setPixiReady(true);
     return app;
   }, []);
 
@@ -258,8 +263,8 @@ export default function WordCloud({
     if (userId == null || userId.length == 0) return;
     console.log("Initializing session...");
     fightSession.current = new FightSession(userId, fightId, () => {
-      console.log("WebSocket open. Getting state...");
-      fightSession.current?.requestGameState();
+      console.log("WebSocket open.");
+      setWebSocketReady(true);
     });
 
     const session = fightSession.current;
@@ -309,6 +314,12 @@ export default function WordCloud({
       fightSession.current?.close();
     };
   }, [fightId, userId]);
+
+  useEffect(() => {
+    if (pixiReady && webSocketReady) {
+      fightSession.current?.requestGameState();
+    }
+  }, [pixiReady, webSocketReady]);
 
   const [remainingTime, setRemainingTime] = useState(0);
   const [timeBeforeStart, setTimeBeforeStart] = useState(0);
@@ -554,7 +565,7 @@ export default function WordCloud({
 
   const pushWord = useCallback(async (word: Word) => {
     if (!refApp.current || !refApp.current.stage) {
-      console.error("App or stage not initialized. Skipping word push.");
+      console.error("App or stage not initialized. Skipping word push. Note that React's strict mode may cause this.");
       return;
     }
     refApp.current.stage.addChild(word.getPixiGraphics());
@@ -565,8 +576,12 @@ export default function WordCloud({
 
   //cleanup function that erases the canvas when the page unmounts
   useEffect(() => {
-    const app = init();
     const currentRefCanvas = refCanvas.current;
+    if (!currentRefCanvas) {
+      console.log("No canvas to use for initialization");
+      return;
+    }
+    const app = init();
 
     return () => {
       app.then((appV) => {
@@ -579,7 +594,7 @@ export default function WordCloud({
         }
       });
     };
-  }, [init]);
+  }, [init, refCanvas]);
 
   /* const updateSize = useCallback(() => { */
   function updateSize() {
